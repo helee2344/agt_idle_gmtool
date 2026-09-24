f3052657e85e0095869b6b5b515b9dc20e141dc1
Heeseong <hslee@aircapseoul.com>
2026-09-24 19:31:48 +0900

    베이스 추가

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('mail_send', {
+    POST_URL_SEND_MAIL: process.env.POST_URL_SEND_MAIL,
+    ADMIN_EXECUTE_FIRST_KEY: process.env.ADMIN_EXECUTE_FIRST_KEY,
+    ADMIN_EXECUTE_SECOND_KEY: process.env.ADMIN_EXECUTE_SECOND_KEY,
+    ADMIN_EXECUTE_THIRD_KEY: process.env.ADMIN_EXECUTE_THIRD_KEY,
+  });
+});
+
+module.exports = router;
