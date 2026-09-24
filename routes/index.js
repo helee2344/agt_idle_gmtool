7102ab5a84db9569132a8a1a435ebbd2200ab162
Heeseong <hslee@aircapseoul.com>
2026-09-24 20:12:34 +0900

    베이스 추가

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/gmtool', function(req, res, next) {
   res.render('mail_send', {
     POST_URL_SEND_MAIL: process.env.POST_URL_SEND_MAIL,
     ADMIN_EXECUTE_FIRST_KEY: process.env.ADMIN_EXECUTE_FIRST_KEY,
